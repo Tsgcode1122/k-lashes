623f102bb6c1e3340b01fef65b6ad36b06aed60d
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-02-10 14:09:23 -0800

    t

--- src/Component/ServiceList.jsx
+++ src/Component/ServiceList.jsx
@@ -5,19 +5,7 @@ import dotted from "../images/curler.png";
 import dotted2 from "../images/curler2.png";
 import { Link } from "react-router-dom";
 import useLeftToRightSwipe from "../animation/useLeftToRightSwipe";
-const workAnimate = {
-  slideOut: { x: -25, opacity: 1 },
-  slideIn: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      bounce: 0.5,
-      delay: 0.1,
-      duration: 1.2,
-    },
-  },
-};
+
 const ServiceList = () => {
   useLeftToRightSwipe(".left-in");
   return (
@@ -32,16 +20,8 @@ const ServiceList = () => {
           Discover lash perfection with our expert services. Classic or Volume,
           we redefine beauty, one lash at a time.
         </article>
-        <motion.div
-          initial={"slideOut"}
-          whileInView={"slideIn"}
-          viewport={{ once: true, amount: 0.2 }}
-          className=" text-center items-center justify-center flex  flex-col md:flex md:flex-wrap md:justify-center md:flex-row md:gap-4"
-        >
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+        <div className=" text-center items-center justify-center flex  flex-col md:flex md:flex-wrap md:justify-center md:flex-row md:gap-4">
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-1 rounded-2xl font-tsg font-tsg">
               Classic & Volume Certification{" "}
               <span className="font-bold ">$900.00 </span>
@@ -50,12 +30,9 @@ const ServiceList = () => {
               Class will start at 9 am! You will learn Classic and Volume
               extensions. Class comes with a kit, manual, and certificate.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl font-tsg font-tsg">
               Lash Removal
               <span className="font-bold "> $20.00 </span>
@@ -64,12 +41,9 @@ const ServiceList = () => {
               Effortlessly shed your lash extensions with our gentle removal
               service, preserving your natural beauty.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl font-tsg">
               Refresher Course
               <span className="font-bold "> $200.00 </span>
@@ -79,11 +53,8 @@ const ServiceList = () => {
               enhance your expertise and keep you at the forefront of lash
               artistry.
             </article>
-          </motion.div>
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          </div>
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl font-tsg">
               Eyebrow Waxing
               <span className="font-bold "> $12.00 </span>
@@ -93,12 +64,9 @@ const ServiceList = () => {
               Eyebrow Waxing service, ensuring precision and a polished look
               that complements your unique features.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl font-tsg">
               Hybrid Lash Full set
               <span className="font-bold "> $12.00 </span>
@@ -107,11 +75,8 @@ const ServiceList = () => {
               Effortlessly shed your lash extensions with our gentle removal
               service, preserving your natural beauty.
             </article>
-          </motion.div>
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          </div>
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl font-tsg">
               Volume Lash Full set
               <span className="font-bold "> $150.00 </span>
@@ -120,12 +85,9 @@ const ServiceList = () => {
               Experience the perfect fusion of classic and volume lashes with
               our Hybrid Lash Full Set, crafted to enhance your natural beauty.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Glam Full set
               <span className="font-bold "> $180.00 </span>
@@ -134,12 +96,9 @@ const ServiceList = () => {
               Unleash glamour in every lash. Elevate your beauty with
               captivating volume and length.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Hybrid Fill <span className="font-bold ">$65.00 </span>
             </p>
@@ -147,11 +106,8 @@ const ServiceList = () => {
               Maintain the perfect balance with our Hybrid Fill. A touch-up for
               stunning lashes.
             </article>
-          </motion.div>
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          </div>
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Volume Fill <span className="font-bold ">$75.00 </span>
             </p>
@@ -159,11 +115,8 @@ const ServiceList = () => {
               Amplify your lashes with our Volume Fill. Enhance your look with
               lavish, voluminous lashes.
             </article>
-          </motion.div>
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          </div>
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Glam Fill <span className="font-bold ">$90.00 </span>
             </p>
@@ -171,12 +124,9 @@ const ServiceList = () => {
               Achieve glamourous allure with our Glam Fill. Elevate your beauty
               with dramatic and full lashes.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Lash Bath <span className="font-bold ">$10.00 </span>
             </p>
@@ -184,12 +134,9 @@ const ServiceList = () => {
               Refresh your lashes with our Lash Bath service for a revitalized
               look.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Strip Lash Adhesive <span className="font-bold ">$5.00 </span>
             </p>
@@ -197,24 +144,18 @@ const ServiceList = () => {
               Secure your strip lashes with our reliable and long-lasting
               adhesive.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Henna Brow and Sculpt <span className="font-bold ">$60.00 </span>
             </p>
             <article>
               Experience defined brows with our Henna Brow and Sculpt service.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Ombre MicroShading (models){" "}
               <span className="font-bold ">$100.00 </span>
@@ -222,24 +163,18 @@ const ServiceList = () => {
             <article>
               Get a stunning ombre look with our MicroShading service.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Bubble Clear Lop Glass <span className="font-bold ">$5.00 </span>
             </p>
             <article>
               For a clear and bubbly finish, choose our Bubble Clear Lop Glass.
             </article>
-          </motion.div>
+          </div>
 
-          <motion.div
-            variants={workAnimate}
-            className="text-black bg-brightC text-center mt-2  mb-2 max-w-[300px] p-4 rounded-2xl font-tsg relative overflow-hidden min-w-[400px]"
-          >
+          <div className="text-black bg-brightC text-center mt-2  mb-2  p-4 rounded-2xl font-tsg relative overflow-hidden max-w-[400px] md:min-w-[200px] md:min-h-[180px] lg:max-w-[350px] lg:min-h-[220px]  ">
             <p className="bg-lightC text-l font-medium inline-block p-2 rounded-2xl">
               Cluster Lashes <span className="font-bold ">$30.00 </span>
             </p>
@@ -247,8 +182,8 @@ const ServiceList = () => {
               Enhance your lashes with the volume and texture of our Cluster
               Lashes.
             </article>
-          </motion.div>
-        </motion.div>
+          </div>
+        </div>
         <Link
           to="Appointment"
           className="pt-4 text-center items-center justify-center"
